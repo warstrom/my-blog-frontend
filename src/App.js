a503f4527f09402390d42b472a3407866ffcaa10
Martin Warstrom <martin.warstrom@devfacto.com>
2022-09-15 10:50:15 -0400

    Frontend commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlesListPage from './pages/ArticlesListPage';
+import ArticlePage from './pages/ArticlePage';
+import NavBar from './components/NavBar';
+import NotFoundPage from './pages/NotFoundPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function App() { 
+  return ( 
+    <Router>
+      <div className="App">
+      <NavBar />
+        <div id="page-body">
+        <Routes> 
+          <Route path="/" element={<HomePage/>} /> 
+          <Route path="/about" element={<AboutPage/>} /> 
+          <Route path="/articles-list" element={<ArticlesListPage/>} /> 
+          <Route path="/article/:name" element={<ArticlePage/>} /> 
+          <Route path="*" element={<NotFoundPage/>} /> 
+        </Routes> 
+        </div>
+      </div> 
+    </Router>
+  ); 
+}   
 
-export default App;
+export default App; 
+
+
+// const ArticlePage = () => { const { name } = useParams(); 
+// const article = articleContent.find(article => name === article.name); 
+// const otherArticles = articleContent.filter(article => name !== article.name);  
---
+++ src/pages/AboutPage.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <>
+      <h1>About me</h1>
+      <p>
+        Doh! Vampires are make-believe, just like elves, gremlins and Eskimos.
+        Even communism works… in theory. I like my beer cold… my TV loud… and my homosexuals flaming.
+        When will I learn? The answer to life’s problems aren’t at the bottom of a bottle, they’re on TV!
+        You’re everywhere. You’re omnivorous.
+        Weaseling out of things is important to learn; it’s what separates us from the animals… except the weasel.
+        Marge, you know it’s rude to talk when my mouth is full. Marge, try to understand.
+        There are two types of college students, jocks and nerds. As a jock, it is my duty to give nerds a hard time.
+        I hope I didn’t brain my damage. Marge it takes two to lie. One to lie and one to listen.
+        Vampires are make-believe, just like elves, gremlins and Eskimos.
+      </p>
+      <p>
+        Doh! Vampires are make-believe, just like elves, gremlins and Eskimos.
+        Even communism works… in theory. I like my beer cold… my TV loud… and my homosexuals flaming.
+        When will I learn? The answer to life’s problems aren’t at the bottom of a bottle, they’re on TV!
+        You’re everywhere. You’re omnivorous.
+        Weaseling out of things is important to learn; it’s what separates us from the animals… except the weasel.
+        Marge, you know it’s rude to talk when my mouth is full. Marge, try to understand.
+        There are two types of college students, jocks and nerds. As a jock, it is my duty to give nerds a hard time.
+        I hope I didn’t brain my damage. Marge it takes two to lie. One to lie and one to listen.
+        Vampires are make-believe, just like elves, gremlins and Eskimos.
+      </p>
+    </>
+  )
+}
+
+export default AboutPage
